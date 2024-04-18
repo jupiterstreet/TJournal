be6be5ba9e8683e7a9308f443dbf2b1a0f244519
Froze-N-Milk <oscar.chevalier@gmail.com>
2024-04-18 16:54:30 +1000

    feat: start of arg parsing + util fns
    - parse the open sub-command
    - using dirs crate to determine '~/.tj/'
    - using chrono to determine current time and format it for filename

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::{env, path::PathBuf};
+
 fn main() {
-	println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let args: Vec<&str> = args.iter().map(|a| a.as_str()).collect();
+    match *args.get(1).expect("Missing first arg") {
+        "open" => open(&args[2..]),
+        _ => todo!("undone")
+    }
+}
+
+fn open(args: &[&str]) {
+    if args.is_empty() {
+        tj_dir().join(local_now_string());
+        return;
+    }
+    ()
+}
+
+fn tj_dir() -> PathBuf {
+    dirs::home_dir().expect("unable to determine home directory").join(".tj")
+}
+
+fn local_now_string() -> String {
+    chrono::Local::now().format("%d-%m-%Y").to_string()
 }
